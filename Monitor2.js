e8b6aca6b17f0cbd612800a2dffb9cec9ac56ae0
Julio Moros <jjmorosr@gmail.com>
2020-10-06 12:31:34 -0400

    adding auto-reconnection options

--- Monitor2.js
+++ Monitor2.js
@@ -11,7 +11,18 @@ class SocketWatch {
     //, url
     //, account
   ) {
-    this.web3ws = new Web3(new Web3.providers.WebsocketProvider(socKet));
+    const options = {
+      // Enable auto reconnection
+      reconnect: {
+        auto: true,
+        delay: 5000, // ms
+        maxAttempts: 5,
+        onTimeout: false,
+      },
+    };
+    this.web3ws = new Web3(
+      new Web3.providers.WebsocketProvider(socKet, options)
+    );
     //this.web3 = new Web3(new Web3.providers.HttpProvider(url));
     //this.account = account.toLowerCase();
   }
@@ -37,11 +48,11 @@ class SocketWatch {
   }
 }
 
-// let sK = "wss://mainnet.sovryn.app:443/ws";
-let sK = "wss://testnet.sovryn.app:443/ws";
+let sK = "wss://mainnet.sovryn.app:443/ws";
+// let sK = "wss://testnet.sovryn.app:443/ws";
 let SckWatch = new SocketWatch(sK);
 let E = "Transfer(address,address,uint256)";
-// let DOC = "0xe700691da7b9851f2f35f8b8182c69c53ccad9db"; // mainnet
-let DOC = "0xcb46c0ddc60d18efeb0e586c17af6ea36452dae0"; // testnet
+let DOC = "0xe700691da7b9851f2f35f8b8182c69c53ccad9db"; // mainnet
+// let DOC = "0xcb46c0ddc60d18efeb0e586c17af6ea36452dae0"; // testnet
 SckWatch.serverListen(DOC, E);
 SckWatch.findTx();
